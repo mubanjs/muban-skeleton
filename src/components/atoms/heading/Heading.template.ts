6399bbfb52b939d445531bc85414defc1d864df5
eelke <eelkevanderbij.prive+github@gmail.com>
2021-01-02 10:21:06 -0300

    Add first setup for heading atom

---
+++ src/components/atoms/heading/Heading.template.ts
@@ -0,0 +1,12 @@
+import { html, unsafeHTML } from '@muban/muban';
+
+type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+export type HeadingTemplateProps = {
+  as: Heading;
+  style: Heading;
+  copy: string;
+};
+
+export const headingTemplate = ({ as, style, copy }: HeadingTemplateProps): string =>
+  html`<${as} class="${style}"  data-component="heading=">${unsafeHTML(copy)}</${as}>`;
