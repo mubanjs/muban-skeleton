582aac2339132e9ce4a1a137d74bc02c1c631fad
Arjan van Wijk <thanarie@gmail.com>
2021-03-25 20:46:41 +0100

    Fix audio/video imports in storybook

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -5,13 +5,27 @@ import { paths } from '../config/paths';
  * the project webpack config, which uses webpack 5!!
  */
 export default ({ config }) => {
-  // remove this rule that deals with svgs
-  config.module.rules = config.module.rules.filter((rule) => !String(rule.test).includes('svg'));
-  // add the rule back without the svg in it, jep, a bit hacky
+  // remove this rule that deals with SVGs and media files
+  config.module.rules = config.module.rules.filter(
+    (rule) => !(String(rule.test).includes('svg') || String(rule.test).includes('mp4')),
+  );
+  // Add the rule back without the svg in it, jep, a bit hacky
   config.module.rules.push({
     test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
-    loader: require.resolve('file-loader'),
+    loader: require.resolve('url-loader'),
     options: {
+      limit: 10000,
+      name: 'static/media/[name].[hash:8].[ext]',
+      esModule: false,
+    },
+  });
+
+  // Re-add the media rule but with esModule turned off, to be in sync with the other assets
+  config.module.rules.push({
+    test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
+    loader: require.resolve('url-loader'),
+    options: {
+      limit: 10000,
       name: 'static/media/[name].[hash:8].[ext]',
       esModule: false,
     },
@@ -26,7 +40,7 @@ export default ({ config }) => {
     oneOf: [
       {
         resourceQuery: /inline/,
-        use: [{ loader: 'svg-inline-loader' }, { loader: 'svgo-loader' }],
+        use: [{ loader: 'raw-loader', options: { esModule: false } }, { loader: 'svgo-loader' }],
       },
       {
         use: [{ loader: 'url-loader' }, { loader: 'svgo-loader' }],
