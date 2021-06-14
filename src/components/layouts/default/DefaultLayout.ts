19f4b0574345edabd8ff72ecb57c5d45fedef3e7
pigeonfresh <eelkevanderbij+github.prive@gmail.com>
2021-06-14 16:19:15 +0200

    Create folders for layouts

--- src/components/layouts/default/DefaultLayout.ts
+++ src/components/layouts/default/DefaultLayout.ts
@@ -1,6 +1,6 @@
 import { defineComponent } from '@muban/muban';
 import { supportLazy } from '@muban/muban/dist/esm/lib/api/apiLazy';
-import { BlockRenderer } from '../block-renderer/BlockRenderer';
+import { BlockRenderer } from '../../block-renderer/BlockRenderer';
 
 export const LayoutDefault = defineComponent({
   name: 'layout-default',
