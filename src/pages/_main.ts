230b62ae7d8ee435ec173987b6a62bf287ad82dc
Arjan van Wijk <thanarie@gmail.com>
2022-01-10 19:34:02 +0100

    Switch to @muban/eslint-config
    And update packages to the latest versions:
    - @muban/template for better error reporting
    - @mediamonks/monck for cli usage

--- src/pages/_main.ts
+++ src/pages/_main.ts
@@ -1,10 +1,10 @@
 export { appTemplate } from '../App.template';
 
-const context = require.context('.', true, /^(?:(?!_).*[\\\/])?(?!_)[^\\\/]+\.ts$/);
+const context = require.context('.', true, /^(?:(?!_).*[\\/])?(?!_)[^\\/]+\.ts$/);
 
 export const pages = Object.fromEntries(
   context
     .keys()
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    .map((filename) => [/\/(.*)\.ts/gi.exec(filename)![1] as string, context(filename)]),
+    .map((filename) => [/\/(.*)\.ts/gi.exec(filename)![1] as string, context(filename)])
 );
